d5a50d315ec6c36eeffcbf04e8477f8d6aca492b
Silavsale <silavsale@gmail.com>
2023-11-14 00:36:59 -0500

    update ubuntu version script

--- Python/script_check_ubuntu_last_version.py
+++ Python/script_check_ubuntu_last_version.py
@@ -1,12 +1,22 @@
+#!/usr/bin/python3
+
 import requests
 import re
 
 def get_latest_ubuntu_version():
-    url = 'https://distrowatch.com/table.php?distribution=ubuntu'
+    url = 'https://ubuntu.com/download/desktop'
     response = requests.get(url)
-    matches = re.findall(r'\d{1,2}\.\d{1,2}', response.text)
-    version_numbers = [float(match) for match in matches]
-    return max(version_numbers)
+    
+    # Regular expression to find version numbers in the format 'XX.XX'
+    # Adjust the regular expression if the format is different
+    matches = re.findall(r'\b\d{2}\.\d{2}\b', response.text)
+    print(matches)
+
+    if matches:
+        # Assuming the first match is the latest version
+        # This might need adjustment based on the actual content of the page
+        return max(matches, key=lambda v: list(map(int, v.split('.'))))
+    else:
+        return "Version not found"
 
 print(get_latest_ubuntu_version())
-
